30258b038d26735e9873b7796eb1366a7c6e3356
Rowan Crawford <rowanc@smx.co.nz>
2010-05-15 23:44:12 +1200

    whups missed out db definition

--- config/environment.rb
+++ config/environment.rb
@@ -9,6 +9,9 @@ RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
+require 'yaml'
+
+db = YAML.load_file('config/database.yml')
 
 Rails::Initializer.run do |config|
   config.action_controller.session = {
